3be273b0dd9345f8b4c561119345ea2601fd5e24
Ítalo Gomes <italopaxg@gmail.com>
2025-10-05 19:14:37 -0300

    feat: get database envs from constants class

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import 'reflect-metadata';
 import "dotenv/config";
 import express from 'express';
-import { AppDataSource } from '@database/config';
-import { initializeEnvs } from '@utils/constants';
+import { CreateAppDataSource } from '@database/config';
+import { getEnv, initializeEnvs } from '@utils/constants';
 
 (async () => {
+  console.log('INITIALIZING ENVS');
   initializeEnvs(process.env);
-
-  const app = express();
+  console.log('INITIALIZED ENVS');
 
   console.log('INITIALIZING DATABASE');
+  const AppDataSource = CreateAppDataSource(getEnv().database);
   await AppDataSource.initialize();
   console.log('INITIALIZED DATABASE');
+
+  const app = express();
 
   app.get('/', (req, res) =>  {
     res.status(200).send({});
--- src/database/config/index.ts
+++ src/database/config/index.ts
@@ -1,17 +1,20 @@
+import { Constants } from '@src/utils/constants';
 import { join } from 'node:path';
 import { DataSource } from 'typeorm';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  database: 'notify-location',
-  password: 'password',
-  migrationsRun: true,
-  migrationsTransactionMode: 'each',
-  migrations: [`${join(__dirname, '../migrations/*.{js,ts}')}`],
-  entities: [`${join(__dirname, '../entities/*.entity.{js,ts}')}`],
-  synchronize: false,
-  logging: true,
-});
+export const CreateAppDataSource = (envs: Constants['database']) => {
+  return new DataSource({
+    type: 'postgres',
+    host: envs.host,
+    port: envs.port,
+    username: envs.username,
+    database: envs.database,
+    password: envs.password,
+    migrationsRun: true,
+    migrationsTransactionMode: 'each',
+    migrations: [`${join(__dirname, '../migrations/*.{js,ts}')}`],
+    entities: [`${join(__dirname, '../entities/*.entity.{js,ts}')}`],
+    synchronize: false,
+    logging: true,
+  })
+};

e2567f56f41a7e92fe0c26eec8a4d7763d0ce224
Ítalo Gomes <italopaxg@gmail.com>
2025-10-16 00:06:36 -0300

    feat: auth middleware

---
+++ src/utils/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { getEnv } from "../constants";
+import { Errors, Unauthorized } from "@utils/errors";
+import { container } from "@utils/inversify/inversify.config";
+import { TYPES } from "@utils/inversify/inversify-types";
+import { IUserRepository } from "@src/database/repositories/interface/user.interface";
+
+const ConstantEnvs = getEnv();
+
+export const auth = async (request: Request, response: Response, next: NextFunction) => {
+  const accessToken = request.headers.authorization;
+
+  if (!accessToken) throw new Unauthorized(Errors.USER_UNAUTHORIZED);
+
+  const userRepository: IUserRepository = container.get(TYPES.repositories.USER_REPOSITORY);
+
+  try {
+    const payload = jwt.verify(accessToken, ConstantEnvs.jwt.secretKey) as jwt.JwtPayload;
+    const user = await userRepository.selectOneOrFail({
+      id: payload.userId,
+    });
+
+    request.session = {
+      userId: user.id,
+    };
+
+    next();
+  } catch (error) {
+    throw new Unauthorized(Errors.USER_UNAUTHORIZED);
+  }
+};

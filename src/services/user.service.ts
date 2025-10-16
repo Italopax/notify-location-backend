2b62b31f95aa3f3574bd81b0adc332eb846f480a
Ítalo Gomes <italopaxg@gmail.com>
2025-10-15 22:08:26 -0300

    feat: create user validations

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,14 +3,36 @@ import { IUserService } from "./interface/user.interface";
 import { inject, injectable } from "inversify";
 import { TYPES } from "@src/utils/inversify/inversify-types";
 import { IUserRepository } from "@src/database/repositories/interface/user.interface";
+import { BadRequest, Errors } from "@src/utils/errors";
+import { validateEmail } from "@src/utils";
+import { EncryptionAdapter } from "@src/adapters/encryption.adapter";
 
 @injectable()
 export class UserService implements IUserService {
   constructor(
-    @inject(TYPES.repositories.USER_REPOSITORY) private readonly userRepository: IUserRepository
+    @inject(TYPES.repositories.USER_REPOSITORY) private readonly userRepository: IUserRepository,
+    @inject(TYPES.adapters.ENCRYPTION_ADAPTER) private readonly bcrypAdapter: EncryptionAdapter,
   ) {}
 
   public async createUser (userData: TUserCreateInput): Promise<Partial<TUserModel>> {
-    return this.userRepository.create(userData);
+    if (
+      !validateEmail(userData.email) ||
+      !userData.name ||
+      !userData.password
+    ) {
+      throw new BadRequest(Errors.INVALID_PARAMS);
+    }
+
+    const userAlreadyExist = await this.userRepository.selectOne({ email: userData.email });
+    if (userAlreadyExist) {
+      throw new BadRequest(Errors.USER_ALREADY_CREATED);
+    }
+
+    const hashPassword = this.bcrypAdapter.generateHash(userData.password);
+
+    return this.userRepository.create({
+      ...userData,
+      password: hashPassword,
+    });
   }
 }
